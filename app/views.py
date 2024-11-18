c2a5327ec280835505bd355fa04c3d8112fdb95a
qrqwqeqt <qrqwqeqt@gmail.com>
2024-11-19 00:13:32 +0200

    add Dockerfile and Dockercompose

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from flask import jsonify
 from app import app
+import time
+
+
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    return jsonify({"status": "OK"}), 200
+    current_time = time.ctime()
+    return jsonify({"status": "OK",
+                    "time": current_time
+                    }), 200
+
---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11.3-slim-bullseye
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN python -m pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+ENV FLASK_APP=app 
+
+CMD ["flask", "run", "--host=0.0.0.0", "--port", "5000"]
